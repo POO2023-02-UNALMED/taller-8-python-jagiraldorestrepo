ccbce89349d838cb6aa6b791d70954a133ba27c1
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-17 11:13:52 -0500

    Create deportista.py

---
+++ deportista.py
@@ -0,0 +1,20 @@
+class Deportista():
+    def __init__(self,añosPracticando):
+        self.__deporte="Futbol"
+        self.__añosPracticando=añosPracticando
+
+
+    def setDeporte(self,deporte):
+        self.__deporte=deporte
+
+    def getDeporte(self):
+        return self.__deporte
+
+
+
+    def setAñosPracticando(self,añosPracticando):
+        self.__añosPracticados=añosPracticando
+
+    def getAñosPracticando(self):
+        return self.__añosPracticando
+   
